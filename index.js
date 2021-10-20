0237340edeb9d0deae9f0ab790f6edd32c287faf
Andrew <alo42@georgetown.edu>
2021-10-20 12:01:19 -0500

    adds express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hi")
+const express = require('express');
+
+const PORT = 8081;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`OpenQ-CoinAPI!`);
+});
+
+app.listen(PORT);
+
+console.log(`Listening on ${PORT}`);
