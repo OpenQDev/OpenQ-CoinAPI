b666c90d3541f13009752a87449bafdf2c3524ab
Andrew <alo42@georgetown.edu>
2021-10-20 20:35:03 -0500

    basic redis server

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const express = require('express');
+const redis = require("redis");
+const axios = require("axios");
+
+const redisPort = 6379;
+const client = redis.createClient(redisPort);
+
+client.on("error", (err) => {
+    console.log(err);
+});
 
 const PORT = 8081;
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send(`OpenQ-CoinAPI!`);
+app.get('/price', (req, res) => {
+    const token = req.query.token;
+    try {
+        client.get(token, async (err, price) => {
+            if (err) throw err;
+
+            if (price) {
+                res.status(200).send({
+                    price,
+                    message: "data retrieved from the cache"
+                });
+            } else {
+                const result = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${token}&vs_currencies=usd`);
+                console.log(result);
+                const coinPrice = result.data[token]["usd"];
+                client.setex(token, 600, coinPrice);
+                res.status(200).send({
+                    price: coinPrice,
+                    message: "cache miss"
+                });
+            }
+        });
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
 });
 
 app.listen(PORT);
