d4dbef988a17be40f72734cfd337149644b180b2
FlacoJones <andrew@openq.dev>
2021-11-16 20:59:25 -0600

    adds an ids endpoint to see supported ids easily

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const redis = require("redis");
-const axios = require("axios");
-require("dotenv").config();
+const express = require('express');
+const redis = require('redis');
+const axios = require('axios');
+require('dotenv').config();
 
 const redisPort = 6379;
 const client = redis.createClient(redisPort, process.env.REDIS_URL);
 
-client.on("error", (err) => {
-  console.log(err);
+client.on('error', (err) => {
+	console.log(err);
 });
 
 const PORT = 8081;
@@ -17,89 +17,96 @@ const app = express();
 	TODO add error cases like in TVL if this API endpoint is going to be used
 */
 
-app.get("/", (req, res) => {
-  const token = req.query.token;
-  console.log("token: ", token);
-  try {
-    client.get(token, async (err, price) => {
-      if (err) throw err;
+app.get('/', (req, res) => {
+	const token = req.query.token;
+	console.log('token: ', token);
+	try {
+		client.get(token, async (err, price) => {
+			if (err) throw err;
 
-      if (price) {
-        res.status(200).send({
-          price,
-          message: "data retrieved from the cache",
-        });
-      } else {
-        const result = await axios.get(
-          `https://api.coingecko.com/api/v3/simple/price?ids=${token}&vs_currencies=usd`
-        );
-        const coinPrice = result.data[token]["usd"];
-        client.setex(token, 600, coinPrice);
-        res.status(200).send({
-          price: coinPrice.toString(),
-          message: "cache miss",
-        });
-      }
-    });
-  } catch (err) {
-    res.status(500).send({ message: err.message });
-  }
+			if (price) {
+				res.status(200).send({
+					price,
+					message: 'data retrieved from the cache',
+				});
+			} else {
+				const result = await axios.get(
+					`https://api.coingecko.com/api/v3/simple/price?ids=${token}&vs_currencies=usd`
+				);
+				const coinPrice = result.data[token]['usd'];
+				client.setex(token, 600, coinPrice);
+				res.status(200).send({
+					price: coinPrice.toString(),
+					message: 'cache miss',
+				});
+			}
+		});
+	} catch (err) {
+		res.status(500).send({ message: err.message });
+	}
 });
 
 // Get mulitple token prices returned
 
-app.get("/tvl", async (req, res) => {
-  if (!req.query.tokens) {
-    res.status(403).json({ error: "missing tokens" });
-    return;
-  }
+app.get('/ids', async (req, res) => {
+	const url = 'https://api.coingecko.com/api/v3/coins/list';
 
-  const tokens = JSON.parse(req.query.tokens);
+	const { data } = await axios.get(url);
+	res.json(data);
+});
 
-  if (!Array.isArray(tokens) || tokens.length === 0) {
-    res.status(403).json({ error: "token must not be empty" });
-    return;
-  }
+app.get('/tvl', async (req, res) => {
+	if (!req.query.tokens) {
+		res.status(403).json({ error: 'missing tokens' });
+		return;
+	}
 
-  const result = await Promise.all(
-    tokens.map(async (token) => {
-      const cachedPrice = await new Promise((resolve, reject) => {
-        client.get(token, (err, price) => {
-          if (err) {
-            reject(err);
-          } else if (price) {
-            resolve(price);
-          } else {
-            console.log(
-              // Is this log possible?
-              "Go and check the code for this case"
-            );
-            resolve(null);
-          }
-        });
-      });
+	const tokens = JSON.parse(req.query.tokens);
 
-      if (cachedPrice) {
-        return cachedPrice;
-      }
+	if (!Array.isArray(tokens) || tokens.length === 0) {
+		res.status(403).json({ error: 'token must not be empty' });
+		return;
+	}
 
-      const url = `https://api.coingecko.com/api/v3/simple/price?ids=${token}&vs_currencies=usd`;
-      try {
-        const { data } = await axios.get(url);
-        const price = data.token.usd;
-        client.setex(token, 600, price);
+	const result = await Promise.all(
+		tokens.map(async (token) => {
+			const cachedPrice = await new Promise((resolve, reject) => {
+				client.get(token, (err, price) => {
+					if (err) {
+						reject(err);
+					} else if (price) {
+						resolve(price);
+					} else {
+						console.log(
+							// Is this log possible?
+							'Go and check the code for this case'
+						);
+						resolve(null);
+					}
+				});
+			});
 
-        return price;
-      } catch (error) {
-        console.error(`failed loading data for ${token}`);
-      }
+			if (cachedPrice) {
+				return cachedPrice;
+			}
 
-      return 0;
-    })
-  );
+			const url = `https://api.coingecko.com/api/v3/simple/price?ids=${token}&vs_currencies=usd`;
+			try {
+				const { data } = await axios.get(url);
+				const price = data.token.usd;
+				client.setex(token, 600, price);
 
-  console.log(result);
-  res.json({ result });
+				return price;
+			} catch (error) {
+				console.error(`failed loading data for ${token}`);
+			}
+
+			return 0;
+		})
+	);
+
+	console.log(result);
+	res.json({ result });
 });
 
 app.listen(PORT);
