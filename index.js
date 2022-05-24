6764ac51a5b6506eb7e5e8b0ca6e1b759c68f01f
FlacoJones <93455288+FlacoJones@users.noreply.github.com>
2022-05-24 09:38:19 -0500

    Revert "Chris"

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const axios = require('axios');
 const main = require('./main');
 const fetchCoinGeckoPrices = require('./utils/fetchCoinGeckoPrices.js');
 const fetchCachedToken = require('./utils/fetchCachedToken.js');
-const metadata = require('./metadata');
-const metadataByToken = require('./metadataByToken');
 
 require('dotenv').config();
 
@@ -14,7 +12,7 @@ require('dotenv').config();
 const redisPort = 6379;
 const client = redis.createClient(redisPort, process.env.REDIS_URL);
 
-client.on('error', err => {
+client.on('error', (err) => {
 	console.log(err);
 });
 
@@ -26,39 +24,10 @@ app.use(cors({ origin: process.env.ORIGIN_URL }));
 
 app.post('/tvl', async (req, res) => {
 	try {
-		const result = await main(
-			req,
-			client,
-			fetchCoinGeckoPrices,
-			fetchCachedToken
-		);
+		const result = await main(req, client, fetchCoinGeckoPrices, fetchCachedToken);
 		res.json(result);
 	} catch (error) {
 		res.status(403).json(error);
-	}
-});
-
-app.get('/tokenMetadata', async (req, res) => {
-	try {
-		const tokenMetadata = await metadata(req, client);
-		const withPath = tokenMetadata.map((token)=>{
-			if((!token.path && !token.logoURI) || token.path?.match(/github.com/) || token.path?.match(/drive.google/) || token.logoURI?.match(/github.com/) || token.logoURI?.match(/drive.google/)){
-				return {...token, path:'/crypto-logos/ERC20.svg', logoURI: '/crypto-logos/ERC20.svg' };
-			}
-			return token;
-		});
-		res.json(withPath);
-	} catch (err) {
-		res.status(500).json(error);
-	}
-});
-
-app.get('/tokenMetadata/:address', async (req, res) => {
-	try {
-		const singleTokenMetadata = await metadataByToken(client, req.params.address);
-		res.json(singleTokenMetadata);
-	} catch (error) {
-		res.status(500).send({ message: error.message });
 	}
 });
 
