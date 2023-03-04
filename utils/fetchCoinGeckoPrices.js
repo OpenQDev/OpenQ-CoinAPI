a1547a17fe4da7b4dd17ada0de88c73d2e20f059
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2023-03-04 21:39:20 +0000

    added api key when available

--- utils/fetchCoinGeckoPrices.js
+++ utils/fetchCoinGeckoPrices.js
@@ -1,20 +1,23 @@
-const axios = require('axios');
+const axios = require("axios");
 
 async function fetchCoinGeckoPrices(client, tokens, network) {
-	const stringifiedTokens = tokens.join(',');
+  const stringifiedTokens = tokens.join(",");
 
-	const url = `https://api.coingecko.com/api/v3/simple/token_price/${network}?contract_addresses=${stringifiedTokens}&vs_currencies=usd`;
-	try {
-		const { data } = await axios.get(url);
+  let url = `https://api.coingecko.com/api/v3/simple/token_price/${network}?contract_addresses=${stringifiedTokens}&vs_currencies=usd`;
+  if (process.env.COINGECK_API_KEY) {
+    url = `${url}&x_cg_pro_api_key=${process.env.COINGECK_API_KEY}`;
+  }
+  try {
+    const { data } = await axios.get(url);
 
-		for (const [key, value] of Object.entries(data)) {
-			client.setex(key, 600, value['usd']);
-		}
-		return data;
-	} catch (error) {
-		console.error(`Failed loading data for ${tokens}: ${error}`);
-		return {};
-	}
+    for (const [key, value] of Object.entries(data)) {
+      client.setex(key, 600, value["usd"]);
+    }
+    return data;
+  } catch (error) {
+    console.error(`Failed loading data for ${tokens}: ${error}`);
+    return {};
+  }
 }
 
 module.exports = fetchCoinGeckoPrices;
