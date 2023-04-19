d9baeb3d579b136fb4f43dfc32e5c44c70c305fe
Christopher Stevers <72156679+Christopher-Stevers@users.noreply.github.com>
2023-04-19 09:31:15 +0000

    added fallback for non-authenticated in coinapi

--- utils/fetchCoinGeckoPrices.js
+++ utils/fetchCoinGeckoPrices.js
@@ -3,15 +3,15 @@ const axios = require("axios");
 async function fetchCoinGeckoPrices(client, tokens, network) {
   const stringifiedTokens = tokens.join(",");
 
-  let url = `https://pro-api.coingecko.com/api/v3/simple/token_price/${network}?contract_addresses=${stringifiedTokens}&vs_currencies=usd`;
+  let url = `https://api.coingecko.com/api/v3/simple/token_price/${network}?contract_addresses=${stringifiedTokens}&vs_currencies=usd`;
   if (process.env.COINGECK_API_KEY) {
-    url = `${url}&x_cg_pro_api_key=${process.env.COINGECK_API_KEY}`;
+    url = `https://pro-api.coingecko.com/api/v3/simple/token_price/${network}?contract_addresses=${stringifiedTokens}&vs_currencies=usd&x_cg_pro_api_key=${process.env.COINGECK_API_KEY}`;
   }
 	
   try {
     const rawData= await axios.get(url);
 
-	const data =	{ ...rawData.data, '0x2791bca1f2de4661ed88a30c99a7a9449aa84174': { usd: 1 }, "0xc2132D05D31c914a87C6611C10748AEb04B58e8F": {usd: 1} };
+  const data =	{ ...rawData.data, '0x2791bca1f2de4661ed88a30c99a7a9449aa84174': { usd: 1 }, "0xc2132D05D31c914a87C6611C10748AEb04B58e8F": {usd: 1} };
     for (const [key, value] of Object.entries(data)) {
       client.setex(key, 600, value["usd"]);
     }
